f2ad6f4cace1786fac782eaec6fca6f56ae580b1
AnesuMurakata <murac96@gmail.com>
2025-02-16 05:59:13 +0200

    handle image input & place detail screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -23,6 +23,11 @@ class PlacesList extends StatelessWidget {
     return ListView.builder(
       itemCount: places.length,
       itemBuilder: (ctx, index) => ListTile(
+        contentPadding: EdgeInsets.all(16),
+        leading: CircleAvatar(
+          radius: 26,
+          backgroundImage: FileImage(places[index].image),
+        ),
         title: Text(
           places[index].title,
           style: Theme.of(context).textTheme.titleMedium!.copyWith(
